c96a84d349d6ef48a93789ed0fca0bc3f0577d85
Altyn <altynbek.shaidyldaev@gmail.com>
2017-04-18 17:36:02 +0600

    Added script file

---
+++ backup-directory-script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Altynbek Shaidyldaev
+
+PATH_TARGET="/target/path"
+PATH_BACKUP="/target/to/backup"
+ARC_NAME="bafe-site"
+NOW="$(date +"%d-%m-%Y_%H-%M")"
+
+echo "archiving files"
+tar czf $PATH_BACKUP/$ARC_NAME-$NOW.tar.gz $PATH_TARGET
+
+if [ $? -ne 0 ]
+then
+    echo "archiving error"
+    exit 13
+    fi
+            
+    echo "removing old archives"
+    find $PATH_BACKUP/$ARC_NAME-*.tar.gz -mtime +20 | xargs rm -f
